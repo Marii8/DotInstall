7aa9aef49ce02181c83f9c03606c56687a5307f6
Mariko Aburai <aburmari@gmail.com>
2018-11-05 21:17:21 +0900

    残り時間が表示されない

--- CountDownTimer/app.js
+++ CountDownTimer/app.js
@@ -11,12 +11,31 @@
     let timeLeft;
     let timeToCountDown = 4 * 1000; //単位を合わせておく
 
+function updateTimer(t) {
+    let d = new Date(t);
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let ms = d.getMilliseconds();
+
+    //桁が足りない時は0で埋めるようにする、桁をsliceで取り出す
+    m = ('0' + m).slice(-2);
+    s = ('0' + s).slice(-2);
+    ms = ('00' + ms).slice(-3);
+
+    timer.textContent = m +':'+ s +'.' + ms;
+}
+
+
+
+
 //setTimeoutを使って、一定時間後に()内の処理を行う。10ミリ秒後に繰り返す
 function countDown(){
     setTimeout(function(){
         timeLeft = timeToCountDown - (Date.now() - startTime);
-        console.log(timeLeft);
+        // console.log(timeLeft);
+        updateTimer(timeLeft);
         countDown(); //countDownを再帰的に呼び出す
+
     }, 10);
     //指定したミリ秒後に、カウントダウンする時間からstartTimeから10ミリ秒ごとの差を出していく
 }
