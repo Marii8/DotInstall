90a4441b78eb94eccb2aaa960c2e568f7b52c3c3
Mariko Aburai <aburmari@gmail.com>
2018-11-05 16:17:02 +0900

    計算の表示を条件訳

--- Calculator/app.js
+++ Calculator/app.js
@@ -6,13 +6,13 @@
 //         }
 
 
-//     let price = document.getElementById('price');
-//     let num = document.getElementById('num');
-//     let unit = document.getElementById('unit');
-//     let btn = document.getElementById('btn');
-//     let result = document.getElementById('result');
-//     let reset = document.getElementById('reset');
-//     let str;
+    let price = document.getElementById('price');
+    let num = document.getElementById('num');
+    let unit = document.getElementById('unit');
+    let btn = document.getElementById('btn');
+    let result = document.getElementById('result');
+    let reset = document.getElementById('reset');
+    let str;
 
 // // input要素のに数値が入っているか確認して、その場合に計算ボタンを有効化する処理
 // // 一桁目には1から9のどれか、
@@ -33,27 +33,25 @@
 //     }
 
 
-//     btn.addEventListener('click', function(){
+    btn.addEventListener('click', function(){
+        let payLess;
+        let short;
+        let payMore;
+        let over;
+        payLess = Math.floor(price.value / num.value / unit.value) * unit.value;
+        short = price.value - (payLess * num.value);
+        payMore = Math.ceil(price.value / num.value / unit.value) *  unit.value;
+        over = Math.abs(price.value - (payMore * num.value));
 
-//         let payLess;
-//         let short;
-//         let payMore;
-//         let over;
-
-//         payLess = Math.floor(price.value / num.value / unit.value) * unit.value;
-//         short = price.value - (payLess * num.value);
-//         payMore = Math.ceil(price.value / num.value / unit.value) *  unit.value;
-//         over = Math.abs(price.value - (payMore * num.value));
-
-//         if (over === 0 && short === 0) {
-//             str = '一人' + (price.value/num.value) + '円ちょうどです！';
-//         } else {
-//             str =
-//                 '一人' + payLess + '円だと' + short + '円足りません。' +
-//                 '一人' + payMore + '円だと' + over + '円余ります。';
-//         }
-//             result.textContent = str;
-//     })
+        if (over === 0 && short === 0) {
+            str = '一人' + (price.value/num.value) + '円ちょうどです！';
+        } else {
+            str =
+                '一人' + payLess + '円だと' + short + '円足りません。' +
+                '一人' + payMore + '円だと' + over + '円余ります。';
+        }
+            result.textContent = str;
+    })
 
 //     price.addEventListener('keyup', checkInput);
 //     num.addEventListener('keyup', checkInput);
