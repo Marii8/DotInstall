3afa5c0206c18a1d099e4f6acb7dc258ab4231c4
Mariko Aburai <aburmari@gmail.com>
2018-11-05 21:02:01 +0900

    jQueryの読み込み追加とsetTimeoutで再帰的にメソッドを呼び出す処理

--- CountDownTimer/app.js
+++ CountDownTimer/app.js
@@ -1,10 +1,30 @@
 (function(){
     'use trict';
 
-    let timer = $('timer');
-    let min = $('min');
-    let sec = $('sec');
-    let reset = $('reset');
-    let start = $('start');
+    let timer = $('#timer');
+    let min = $('#min');
+    let sec = $('#sec');
+    let reset = $('#reset');
+    let start = $('#start');
 
+    let startTime;
+    let timeLeft;
+    let timeToCountDown = 4 * 1000; //単位を合わせておく
+
+//setTimeoutを使って、一定時間後に()内の処理を行う。10ミリ秒後に繰り返す
+function countDown(){
+    setTimeout(function(){
+        timeLeft = timeToCountDown - (Date.now() - startTime);
+        console.log(timeLeft);
+        countDown(); //countDownを再帰的に呼び出す
+    }, 10);
+    //指定したミリ秒後に、カウントダウンする時間からstartTimeから10ミリ秒ごとの差を出していく
+}
+
+
+
+    start.on('click', function(){
+        startTime = Date.now();
+        countDown(); //残り時間を表示させる
+    })
 }) ();
